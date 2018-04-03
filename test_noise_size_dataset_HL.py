3aa1713a858fdb410844a3098a3e5d22937ab5e2
FabienLab <fabien.labernia@gmail.com>
2018-04-03 10:26:12 +0200

    update test results

--- test_noise_size_dataset_HL.py
+++ test_noise_size_dataset_HL.py
@@ -3,7 +3,7 @@ from src.learnCPnet import *
 modeForDatasetGeneration = 2 # 1 = read a file, 2 = generate a synthetic database
 nameOfFile = "databases/sushi_30Users_10000Comparisons.data"
 numberOfComparisons = [5000,50000,200000] # -1 = all of the comparisons in file
-percentageOfNoise = [0] # between 0 and 50
+percentageOfNoise = [0,10] # between 0 and 50
 numberOfVariables = 12 # -1 = automatically choose from the number of comparisons
 numberOfEdgesLambda = -1 # -1 = infinity
 numberOfParentsForTargetCPNet = -1 # -1 = infinity
