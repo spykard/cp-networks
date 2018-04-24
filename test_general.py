52d5a567f922174300917b2e961d8bcecc6dea4c
Fabien Labernia <fabien.labernia@gmail.com>
2018-04-24 11:55:15 +0200

    update test results

--- test_general.py
+++ test_general.py
@@ -4,14 +4,14 @@ from src.learnCPnet import *
 # arguments for python3 command line: numberOfRounds online[True or False] offline[True or False] decisionMode[1 or 2] decisionThresholdBis epsilonThreshold numberOfComparisons
 print(argv[1],argv[2],argv[3],argv[4],argv[5],argv[6],argv[7])
 
-modeForDatasetGeneration = 2 # 1 = read a file, 2 = generate a synthetic database
-nameOfFile = "databases/test_database.dat"
+modeForDatasetGeneration = 1 # 1 = read a file, 2 = generate a synthetic database
+nameOfFile = "databases/movieLensDataset_user4222.dat"
 numberOfComparisons = int(argv[7]) # -1 = all of the comparisons in file
 percentageOfNoise = [0] # between 0 and 50
-numberOfVariables = 12 # -1 = automatically choose from the number of comparisons
+numberOfVariables = -1 # -1 = automatically choose from the number of comparisons
 numberOfEdgesLambda = -1 # -1 = infinity
 numberOfParentsForTargetCPNet = -1 # -1 = infinity
-numberOfParentsForLearnedCPNet = -1 # -1 = infinity
+numberOfParentsForLearnedCPNet = 4 # -1 = infinity
 numberOfRoundsForFileGeneration = 1
 numberOfRoundsForLearningProcedure = int(argv[1]) # = percentage taken in the dataset for the cross validation
 
