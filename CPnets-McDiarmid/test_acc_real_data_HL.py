2382b6c730d32e9d5ceeff0d01bee2758da5985c
spykard <kardakis@ceid.upatras.gr>
2019-11-22 05:18:47 +0200

    From relative path to real path

--- CPnets-McDiarmid/test_acc_real_data_HL.py
+++ CPnets-McDiarmid/test_acc_real_data_HL.py
@@ -1,7 +1,8 @@
 from src.learnCPnet import *
+import os.path
 
 modeForDatasetGeneration = 1 # 1 = read a file, 2 = generate a synthetic database
-nameOfFile = [None,None,None,None,"databases/simple_abcd_test_michael_et_al.dat"]
+nameOfFile = [None,None,None,None,os.path.abspath("CPnets-McDiarmid/databases/simple_abcd_test_michael_et_al.dat")]
 # nameOfFile = ["databases/sushi_30Users_10000Comparisons.dat","databases/sushi_30Users_20000Comparisons.dat","databases/hotels_parsing_binarisation_10000.dat","databases/hotels_parsing_binarisation_20000.dat","databases/movieLensDataset_200000.dat"]
 numberOfComparisons = -1 # -1 = all of the comparisons in file
 percentageOfNoise = [0] # between 0 and 50
@@ -62,7 +63,7 @@ decisionMode = 2
 # 	fileTestHL.write(str(numberOfParentsForLearnedCPNet) + " " + str(aOffline[0]) + " " + str(sdAOffline[0]) + " " + str(aOfflineLog[0]) + " " + str(sdAOfflineLog[0]) + "\n")
 # fileTestHL.close()
 
-fileTestHL = open("test-results/test_acc_simple_abcd_test.dat","w")
+fileTestHL = open(os.path.abspath("CPnets-McDiarmid/test-results/test_acc_simple_abcd_test.dat"),"w")
 for numberOfParentsForLearnedCPNet in [3]:
 	averageCycleSize2,aOnline,aOnlineLog,sdAOnline,sdAOnlineLog,aOffline,aOfflineLog,sdAOffline,sdAOfflineLog,tOnline,sdTOnline,meanIT,sdIT,tOffline,sdTOffline,meanAccNoiseOnline,meanAccNoiseOnlineLog,sdANoiseOnline,sdANoiseOnlineLog,meanAccNoiseOffline,meanAccNoiseOfflineLog,sdANoiseOffline,sdANoiseOfflineLog,lenOfFold,numberOfAttributes,meanConvergenceAccuracyOnline,meanConvergenceAccuracyOnlineLog,sdConvergenceAccuracyOnline,sdConvergenceAccuracyOnlineLog,meanConvergenceAccuracyOffline,meanConvergenceAccuracyOfflineLog,sdConvergenceAccuracyOffline,sdConvergenceAccuracyOfflineLog = generalProcedure(modeForDatasetGeneration,nameOfFile[4],numberOfComparisons,percentageOfNoise,numberOfVariables,numberOfEdgesLambda,numberOfParentsForTargetCPNet,numberOfParentsForLearnedCPNet,numberOfRoundsForFileGeneration,numberOfRoundsForLearningProcedure,decisionThresholdBis,epsilonThreshold,convergence,online,offline,decisionMode,None,autorizedCycle)
 	fileTestHL.write(str(numberOfParentsForLearnedCPNet) + " " + str(aOffline[0]) + " " + str(sdAOffline[0]) + " " + str(aOfflineLog[0]) + " " + str(sdAOfflineLog[0]) + "\n")
